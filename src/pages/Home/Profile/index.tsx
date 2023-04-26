ea508eacec05b437910baae1b59e37a87fd60574
Matheus Borbely <matheus.silvaborbely@gmail.com>
2023-04-25 22:28:00 -0300

    create profile component

--- src/pages/Home/Profile/index.tsx
+++ src/pages/Home/Profile/index.tsx
@@ -1,6 +1,9 @@
 import {useEffect, useState} from 'react'
 import axios from '../../../lib/axios';
-import { ProfileContainer } from "./styles";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBuilding, faArrowUpRightFromSquare, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import {  faGithub } from '@fortawesome/free-brands-svg-icons'
+import { ProfileContainer, ProfileDetails, ProfileImageAvatar, ProfileInfo } from "./styles";
 import { Routes } from '../../../utils/routes.enum';
 
 interface Profile {
@@ -9,22 +12,33 @@ interface Profile {
     bio: string,
     nickname: string,
     company:string,
+    url: string,
     followers: number
 }
 
 export function Profile() {
-    const [profile, setProfile] = useState<Profile>();
+    const [profile, setProfile] = useState<Profile>({
+        image: '',
+        name: '',
+        bio: '',
+        nickname: '',
+        company:'',
+        url: '',
+        followers: 0
+    });
 
-    /*async function searchUser() {
+    async function searchUser() {
         try {
 
-            const response = await axios.get<Profile>(`${Routes.USER}/MatheusBorbely`);
-            const {avatar_url, login, name, bio, company, followers} = response;
+            const response = await axios.get(`${Routes.USER}/MatheusBorbely`);
+            console.log(response)
+            const {avatar_url, login, html_url, name, bio, company, followers} = response.data;
 
             setProfile( {
                 image: avatar_url,
-                name: login,
-                nickname: name,
+                nickname: login,
+                url: html_url,
+                name, 
                 bio,
                 company,
                 followers
@@ -36,11 +50,53 @@ export function Profile() {
 
     useEffect(() => {
         searchUser();
-    }, []);*/
+    }, []);
 
+    const followers = profile.followers <= 1 ? 'seguidor' : 'seguidores'
     return (
         <ProfileContainer>
-            <img  alt="" />
+            <ProfileImageAvatar>
+                <img  src={profile.image} alt={`Imagem do perfil do ${profile.name}`}/>
+            </ProfileImageAvatar>
+            <ProfileInfo>
+                <header>
+                    <h1>
+                        {profile.name}
+                    </h1>
+                    <a href={profile.url} target="_blank" rel="noopener noreferrer">
+                        github
+                        <FontAwesomeIcon icon={faArrowUpRightFromSquare} color='#3294F8' size='xs'/>
+                    </a>
+                </header>
+                <article>
+                    <p>
+                        {profile.bio}
+                    </p>
+                </article>
+                <div>
+                    <ProfileDetails>
+                        <FontAwesomeIcon icon={faGithub} color='#3A536B' size='lg'/>
+                        <span>
+                            {profile.nickname}
+                        </span>
+                    </ProfileDetails>
+
+                    <ProfileDetails>
+                        <FontAwesomeIcon icon={faBuilding} color='#3A536B' size='lg'/>
+                        <span>
+                            {profile.company}
+                        </span>
+                    </ProfileDetails>
+                    
+                    <ProfileDetails>
+                        <FontAwesomeIcon icon={faUserGroup} color='#3A536B' size='lg'/>
+                        <span>
+                            {`${profile.followers} ${followers}`}
+                        </span>
+                    </ProfileDetails>
+                </div>
+            </ProfileInfo>
+            
         </ProfileContainer>
     )
 }
--- src/pages/Home/Profile/styles.ts
+++ src/pages/Home/Profile/styles.ts
@@ -1,8 +1,76 @@
 import styled from 'styled-components';
 
 export const ProfileContainer = styled.section`
+    display: flex;
+    gap: 2rem;
     width: 100%;
     padding: 2rem;
     border-radius: 10px;
+    position: relative;
+    transform: translateY(-43%);
     background-color: ${(props) => props.theme['base-profile']};
+`
+export const ProfileImageAvatar= styled.div`
+    position: relative;
+    width: 100%;
+    padding-top: 19.25%;
+    max-width: 148px;
+    max-height: 148px;
+
+    img{
+        position: absolute;
+        top: 0;
+        left:0;
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        border-radius: 8px;
+    }
+`
+export const ProfileInfo = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+
+    header{
+        display: flex;
+        justify-content: space-between;
+        padding-top: .5rem;
+        h1{
+            font-weight: 700;
+            font-size: 1.5rem;
+            color: ${props => props.theme['base-title']}
+        }
+        a{
+            display: flex;
+            align-items: center;
+            text-transform: uppercase;
+            font-size: 0.75rem;
+            gap: .5rem;
+            color: ${props => props.theme.blue};
+        }
+    }
+    article{
+        p {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+    }
+    div{
+        display: flex;
+        gap: 2rem;
+        padding-top: 1rem;
+    }
+
+`
+export const ProfileDetails = styled.section`
+    display: flex;
+    gap: .5rem;
+    svg {
+        width: 18px;
+        height: 18px;
+    }
 `
